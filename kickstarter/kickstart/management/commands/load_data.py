bb5bf7b96464086cbd56c09f4d19664ac85a604e
Casey <crhird@g.clemson.edu>
2018-12-20 15:56:01 -0500

    Finish kickstarter api project.

--- kickstarter/kickstart/management/commands/load_data.py
+++ kickstarter/kickstart/management/commands/load_data.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from models.py import kickstarter
+from kickstart.models import kickstarter
+import csv
 
 class Command(BaseCommand):
     help = 'Load csv file.'
@@ -8,6 +8,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--path', type=str)
 
+    kickstarter.objects.all().delete()
     def handle(self, *args, **kwargs):
         path = kwargs['path']
         with open(path, 'rt') as f:
@@ -16,10 +17,10 @@ class Command(BaseCommand):
             for row in reader:
                 if count is 0:
                     count += 1
-                    continue    
+                    continue
                 kickstarter.objects.create(
-                    backers_count = row[0]
-                    blurb = row[1]
-                    category = row[2]
+                    backers_count = row[0],
+                    blurb = row[1],
+                    category = row[2],
                     converted_pledged_amount = row[3]
                 )
